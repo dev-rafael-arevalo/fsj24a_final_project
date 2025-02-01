5d2c03cb048deb197af4ba57b9368728e34e713f
dev-rafael-arevalo <rafael.edgardo.arevalo@gmail.com>
2025-01-31 19:53:38 -0600

    quitar comentarios del modelo de usuarios

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,35 +10,19 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
         'name',
         'email',
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
@@ -46,17 +30,12 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
-    /**
-     * Relación con las reviews del usuario.
-     */
+
     public function reviews()
     {
         return $this->hasMany(Review::class);
     }
 
-    /**
-     * Calcular el promedio de valoraciones.
-     */
     public function averageRating()
     {
         return $this->reviews()->avg('rating');
